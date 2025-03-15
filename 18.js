510c41365921a7210067d8b39a0b8b74b75c690b
Lạp Hộ <lap17022005@gmail.com>
2025-03-15 08:05:35 +0700

    Update 18.js

--- 18.js
+++ 18.js
@@ -1,2 +1,5 @@
-let body = $response.body.replace(/"YouTube"/g, '"YouTube Premium"');
+let body = $response.body;
+if (body.includes('"YouTube"')) {
+    body = body.replace(/"YouTube"/g, '"YouTube Premium"');
+}
 $done({body});
